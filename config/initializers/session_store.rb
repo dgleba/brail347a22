d71a39700ef074520efa4a83e00fd01513368f2b
David Glebagit <dgleba@gmail.com>
2019-06-16 18:38:23 -0500

    2019-06-16 set session cookie differently for production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,17 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_brail347a22_session'
+# rails-env not set yet.
+# Rails.application.config.session_store :cookie_store, key: "_brail347a22_session_#{RAILS_ENV}"
+
+# 2019-06-16_Sun_19.28-PM set different session for each env..
+Rails.application.config.session_store :cookie_store, key: "_brail347a22_session_#{Rails.env}"
+
+puts  Rails.env.to_sym, :all
+puts  " rails.env.23 #{Rails.env}"
+
+# 2019-06-16 
+# Rails.application.config.session_store :cookie_store, key: '_brail347a22_subsession', domain: {
+#   production: '.example.com',
+#   development: '.example.dev'
+# }.fetch(Rails.env.to_sym, :all)
+
